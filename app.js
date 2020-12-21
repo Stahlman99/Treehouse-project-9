a55cb21d8788e8ce1d245f5f7f809835bdcb377c
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-12-21 13:55:47 -0600

    Add working connection to the database.
    I changed the config.js file, and my connection now works as expected.

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const morgan = require('morgan');
 const routes = require('./routes');
 const Sequelize = require('sequelize');
 
-// Create instance of Sequelize.
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'fsjstd-restapi.db'
-});
-
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
